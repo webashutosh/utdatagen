fd6b8a21553496526f0b34581c753ab8a2ce7bd9
Ashutosh Agrawal <webashutosh@gmail.com>
2018-03-11 17:44:26 +0530

    Initial Commit

---
+++ src/main/java/in/acode/utdatagen/UtdatagenApplication.java
@@ -0,0 +1,12 @@
+package in.acode.utdatagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtdatagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtdatagenApplication.class, args);
+	}
+}
